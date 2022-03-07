bfe4aa6e97cf84dd5707694a243612052cf01ae9
Oleksandr Zinkevych <zso040399@gmail.com>
2022-03-07 18:14:38 +0200

    Add http json get method

--- go.mod
+++ go.mod
@@ -3,6 +3,17 @@ module proxy-checker-go
 go 1.17
 
 require (
+	github.com/buaazp/fasthttprouter v0.1.1
+	github.com/json-iterator/go v1.1.12
 	github.com/pkg/errors v0.9.1
+	github.com/valyala/fasthttp v1.33.0
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.14.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,57 +7,74 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
+	"github.com/buaazp/fasthttprouter"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/pkg/errors"
+	"github.com/valyala/fasthttp"
 	"golang.org/x/net/proxy"
 )
 
-type Proxy struct {
+type ProxyConnectionData struct {
 	Ip   string
-	Port string
+	Port uint32
 	Auth *proxy.Auth
 }
 
-func (p Proxy) Addr() string {
-	return fmt.Sprintf("%s:%s", p.Ip, p.Port)
+func (p ProxyConnectionData) Addr() string {
+	return fmt.Sprintf("%s:%v", p.Ip, p.Port)
 }
 
-func ParseProxy(s string) (Proxy, error) {
-	host, port, err := net.SplitHostPort(s)
+func ParseProxy(s string) (ProxyConnectionData, error) {
+	host, portStr, err := net.SplitHostPort(s)
 	if err != nil {
-		return Proxy{}, errors.WithStack(err)
+		return ProxyConnectionData{}, errors.WithStack(err)
 	}
-	return Proxy{Ip: host, Port: port}, nil
+	port, err := strconv.ParseUint(portStr, 10, 32)
+	if err != nil {
+		return ProxyConnectionData{}, errors.WithStack(err)
+	}
+	return ProxyConnectionData{Ip: host, Port: uint32(port)}, nil
 }
 
 type ProxyDB interface {
-	Add(proxy Proxy) error
-	Proxies() chan Proxy
+	Add(proxy *ProxyInfo) error
+	Proxies() chan *ProxyInfo
+	Refresh()
 }
 
 type LocalProxyDB struct {
-	proxies map[Proxy]struct{}
-	lock    sync.RWMutex
+	proxies       map[*ProxyInfo]struct{}
+	byCountryCode map[string]map[*ProxyInfo]struct{}
+	lock          sync.RWMutex
 }
 
 func NewProxyDB() *LocalProxyDB {
 	return &LocalProxyDB{
-		proxies: map[Proxy]struct{}{},
-		lock:    sync.RWMutex{},
+		proxies:       map[*ProxyInfo]struct{}{},
+		byCountryCode: map[string]map[*ProxyInfo]struct{}{},
+		lock:          sync.RWMutex{},
 	}
 }
 
-func (pdb *LocalProxyDB) Add(proxy Proxy) error {
+func (pdb *LocalProxyDB) Add(proxy *ProxyInfo) error {
 	pdb.lock.Lock()
 	defer pdb.lock.Unlock()
 	pdb.proxies[proxy] = struct{}{}
+	proxies, ok := pdb.byCountryCode[proxy.CountryCode]
+	if ok {
+		proxies[proxy] = struct{}{}
+	} else {
+		pdb.byCountryCode[proxy.CountryCode] = map[*ProxyInfo]struct{}{proxy: {}}
+	}
 	return nil
 }
 
-func (pdb *LocalProxyDB) Proxies() chan Proxy {
-	ch := make(chan Proxy)
+func (pdb *LocalProxyDB) Proxies() chan *ProxyInfo {
+	ch := make(chan *ProxyInfo)
 	go func() {
 		pdb.lock.RLock()
 		defer pdb.lock.RUnlock()
@@ -74,31 +91,48 @@ type ProxyCheckResponse struct {
 	CountryCode string
 }
 
-func checkProxy(p Proxy) error {
+type ProxyInfo struct {
+	proxy       ProxyConnectionData
+	CountryCode string
+}
+
+func (p *ProxyInfo) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Ip          string `json:"ip"`
+		Port        uint32 `json:"port"`
+		CountryCode string `json:"countryCode"`
+	}{
+		Ip:          p.proxy.Ip,
+		Port:        p.proxy.Port,
+		CountryCode: p.CountryCode,
+	})
+}
+
+func checkProxy(p ProxyConnectionData) (*ProxyInfo, error) {
 	dialer, err := proxy.SOCKS5("tcp", p.Addr(), nil, proxy.Direct)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "can't connect to the proxy:", err)
-		return errors.WithStack(err)
+		return nil, errors.WithStack(err)
 	}
 	httpTransport := &http.Transport{}
 	client := &http.Client{Transport: httpTransport, Timeout: 10 * time.Second}
 	httpTransport.Dial = dialer.Dial
 	resp, err := client.Get("http://ip-api.com/json")
 	if err != nil {
-		return errors.WithStack(err)
+		return nil, errors.WithStack(err)
 	}
 	defer resp.Body.Close()
 	decoder := json.NewDecoder(resp.Body)
 	r := new(ProxyCheckResponse)
 	decoder.Decode(r)
 	if r.Query != p.Ip {
-		return errors.New("Ip mismatch")
+		return nil, errors.New("Ip mismatch")
 	}
-	return nil
+	return &ProxyInfo{proxy: p, CountryCode: r.CountryCode}, nil
 }
 
-func getProxies() chan Proxy {
-	ch := make(chan Proxy)
+func getProxies() chan ProxyConnectionData {
+	ch := make(chan ProxyConnectionData)
 	go func() {
 		client := http.Client{Timeout: time.Second * 10}
 		resp, err := client.Get("http://localhost:8000/socks5.txt")
@@ -120,25 +154,67 @@ func getProxies() chan Proxy {
 	return ch
 }
 
-func main() {
-	db := NewProxyDB()
+func (pdb *LocalProxyDB) Refresh() {
+	newDB := NewProxyDB()
 	var wg sync.WaitGroup
 	for p := range getProxies() {
 		wg.Add(1)
 		p := p
-		go func ()  {
-			err := checkProxy(p)
+		go func() {
+			proxyInfo, err := checkProxy(p)
 			if err != nil {
 				// fmt.Println(err)
 			} else {
-				// fmt.Printf("ADDED! %v\n", p)
-				db.Add(p)
+				fmt.Printf("ADDED! %v\n", p)
+				newDB.Add(proxyInfo)
 			}
 			wg.Done()
 		}()
 	}
 	wg.Wait()
-	for p := range db.Proxies() {
-		fmt.Println(p)
+	pdb.lock.Lock()
+	defer pdb.lock.Unlock()
+	pdb.proxies = newDB.proxies
+	pdb.byCountryCode = newDB.byCountryCode
+}
+
+type JsonHandler struct {
+	db ProxyDB
+}
+
+func (jh JsonHandler) HandleGet(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("application/json")
+	s := jsoniter.NewStream(jsoniter.ConfigDefault, ctx, 0)
+	s.WriteArrayStart()
+	ch := jh.db.Proxies()
+	p, ok := <-ch
+	if ok {
+		for {
+			s.WriteVal(p)
+			p, ok = <-ch
+			if ok {
+				s.WriteMore()
+			} else {
+				break
+			}
+			s.Flush()
+		}
 	}
+	s.WriteArrayEnd()
+	s.Flush()
+}
+
+func runServer(addr string, db ProxyDB) {
+	router := fasthttprouter.New()
+	router.GET("/json", JsonHandler{db}.HandleGet)
+	fasthttp.ListenAndServe(addr, router.Handler)
+}
+
+func main() {
+	db := NewProxyDB()
+	fmt.Println("Refreshing...")
+	go db.Refresh()
+	addr := ":8080"
+	fmt.Printf("Running at %s\n", addr)
+	runServer(addr, db)
 }
